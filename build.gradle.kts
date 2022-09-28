a6adda64f0de838ae5d97d3485815b064b453521
Anderson Xavier <anderson.xavier91@gmail.com>
2022-09-28 01:08:43 -0300

    include jacoco on gradlew.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
+	jacoco
 }
 
 group = "br.com.bank"
